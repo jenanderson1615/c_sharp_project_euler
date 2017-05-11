9ce277eaf11f376ac2f8f7d104cfe8c67d5dfa52
Jennifer Loecker <jennifer@macpractice.com>
2017-05-11 11:05:57 -0500

    Renamed the project Multiple_Sum because I read the description wrong

---
+++ Multiple_Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Multiple_Sum
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int primeTotal = calculatePrimeTotal();
+			Console.WriteLine($"The total of the primes is: {primeTotal}");
+		}
+
+		static int calculatePrimeTotal()
+		{
+			int primeTotal = 0;
+			return primeTotal;
+		}
+	}
+}
+
