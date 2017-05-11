fb21037e18917bd7095befb0756413ac58f58b51
Jennifer Loecker <jennifer@macpractice.com>
2017-05-11 11:52:54 -0500

    Removed the list creation as a step and added the total calculation.  This passed on the website

--- Multiple_Sum/Program.cs
+++ Multiple_Sum/Program.cs
@@ -6,50 +6,30 @@ namespace Multiple_Sum
     class Program
     {
         static int upperLimit = 1000;
+        static int total = 0;
 
 		static void Main(string[] args)
 		{
-           List<int> myList = createMultiplesList();
-            printMultiplesList(myList);
-            int primeTotal = calculatePrimeTotal();
-			Console.WriteLine($"The total of the primes is: {primeTotal}");
+            calculatePrimeTotal();
+			Console.WriteLine($"The total of the primes is: {total}");
 		}
 
-		static int calculatePrimeTotal()
-		{
-			int primeTotal = 0;
-			return primeTotal;
-		}
-
-		//
-		// Loop through all the values up to upper limit and add to an array if N % 3 or N % 5 is 0
-        //
-        static List<int> createMultiplesList()
+        static void calculatePrimeTotal()
         {
-           List<int> someInts = new List<int>(); ;
             for (int i = 1; i < upperLimit; i++)
             {
                 if (i % 3 == 0)
                 {
-                    someInts.Add(i);
+                    total += i;
                     continue;
                 }
                 if (i % 5 == 0)
                 {
-                    someInts.Add(i);
+                    total += i;
                     continue;
                 }
             }
-            return someInts;
         }
-
-        static void printMultiplesList(List<int> myList)
-		{
-            foreach (int item in myList)
-			{
-                Console.WriteLine($"{item}");
-			}
-		}
 	}
 }
 
