b7235cfe9f1e1099b053250f61843d50eba81edb
Jennifer Loecker <jennifer@macpractice.com>
2017-05-11 13:14:55 -0500

    Even Fib  number summary

---
+++ Even_Fib_Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Even_Fib_Sum
+{
+    class Program
+    {
+        static int upperLimit = 4000000;
+        static int firstNumber = 1;
+        static int secondNumber = 2;
+        static int totalOfNumbers = 2;
+        
+        static void Main(string[] args)
+        {
+            createFibNumberList();
+            Console.WriteLine($"{totalOfNumbers}");
+        }
+
+        static void createFibNumberList()
+        {
+            int thirdNumber = firstNumber + secondNumber;
+            while (thirdNumber < upperLimit)
+            {
+                if (thirdNumber % 2 == 0)
+                {
+                    totalOfNumbers += thirdNumber;
+                }
+                firstNumber = secondNumber;
+                secondNumber = thirdNumber;
+                thirdNumber = firstNumber + secondNumber;
+            }
+        }
+    }
+}
