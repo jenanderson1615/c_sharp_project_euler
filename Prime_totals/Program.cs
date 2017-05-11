e87a9fdb7eda63d6f72621bfac7c973fa2063a54
Jennifer Loecker <jennifer@macpractice.com>
2017-05-11 10:29:05 -0500

    Added the basic file setup and changed the message

---
+++ Prime_totals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prime_totals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The total of the primes is: ");
+        }
+    }
+}
